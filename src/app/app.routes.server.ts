6dcf6d95fac62e3643689cd7ac5934e9f7f8dfa0
benjaminlsk <73712469+benjaminlsk@users.noreply.github.com>
2026-06-20 20:14:05 +0200

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'chat/:userId',
+    renderMode: RenderMode.Server
+  }
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
